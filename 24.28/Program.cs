6441a1792c84f71d89f3e0b3fa00a16df0950cb1
acer <acer@LAPTOP-H9QES7A9>
2024-05-15 14:18:59 +0200

    Aggiungere i file di progetto.

---
+++ 24.28/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Resistenza r1 = new Resistenza(220);
+            Resistenza r2 = new Resistenza(220);
+            Console.WriteLine(r1 & r2);
+            Console.WriteLine(r2 & r1);
+
+         
+
+        }
+    }
+}
---
+++ 24.28/Resistenza.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Cache;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._28
+{
+    internal class Resistenza
+    {
+        public double Valore { get; set; }
+
+        public Resistenza() { }
+        public Resistenza(double valore)
+        {
+            Valore = valore;            
+        }
+
+        public static Resistenza operator &(Resistenza r1, Resistenza r2)
+        {
+            return new Resistenza(r1.Valore + r2.Valore);
+        }
+        public static Resistenza operator |(Resistenza r1, Resistenza r2)
+        {
+            return new Resistenza(1/((1/r1.Valore) + (1/r2.Valore)));
+        } 
+        public static bool operator ==(Resistenza r1, Resistenza r2)  //confronto di uguaglianza 
+        {
+            if (object.ReferenceEquals(r1, null))
+                return object.ReferenceEquals(r2, null);
+            else if (object.ReferenceEquals(r2, null))
+                return false;
+            else 
+                return r2.Valore == r1.Valore;
+        }
+        public static bool operator !=(Resistenza r1, Resistenza r2)
+        {
+            return !(r1.Valore == r2.Valore);
+        }
+
+        public static bool operator >(Resistenza r1, Resistenza r2)
+        {
+            return r1.Valore > r2.Valore;
+        }
+        public static bool operator <(Resistenza r1, Resistenza r2) 
+        {
+            return r1.Valore < r2.Valore;
+        }
+        public override string ToString()
+        {
+            return String.Format($"{0}", Valore);
+        }
+        public static Resistenza Parse(string str)
+        {
+            string[] parts = str.Split();
+
+            return new Resistenza(double.Parse(parts[0]));
+
+        }
+
+
+
+    }
+}
